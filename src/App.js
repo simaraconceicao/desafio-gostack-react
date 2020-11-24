c64eecc23a2893fec55901700da17b127a07587e
Simara Conceição <contato@simaraconceicao.com>
2020-11-24 11:28:23 -0300

    entrega desafio

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from './services/api'
 
 import "./styles.css";
 
 function App() {
+  const [ repositories, setRepositories ] = useState([])
+
+  useEffect(()=>{
+    async function getData(){
+      const response = await api.get('repositories')
+      const data = await response.data
+      setRepositories(data)
+    }
+    getData()
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: 'Meu Desafio ReactJS',
+      url: 'https://github.com/siconceicao',      
+      techs: ['React', 'Node.js']
+    })
+    setRepositories([ ...repositories, response.data ])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`)
+
+    setRepositories(repositories.filter(repo =>{
+      return repo.id !== id
+    }))
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.map(repo =>{
+          return <li key={repo.id}>
+          {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
+          <button onClick={() => handleRemoveRepository(repo.id)}>
             Remover
           </button>
         </li>
+        })}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
